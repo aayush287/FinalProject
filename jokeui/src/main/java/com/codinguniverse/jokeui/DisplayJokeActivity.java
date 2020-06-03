30c20f6eac3516b91fd7642a7e6dbb60379a79c7
caayush36 <aayush.chaudhary@fiellements.com>
2020-06-03 13:27:53 +0530

    Adding Android Library to show joke

--- jokeui/src/main/java/com/codinguniverse/jokeui/DisplayJokeActivity.java
+++ jokeui/src/main/java/com/codinguniverse/jokeui/DisplayJokeActivity.java
@@ -2,13 +2,23 @@ package com.codinguniverse.jokeui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DisplayJokeActivity extends AppCompatActivity {
+
+    public static final String EXTRA_JOKE = "joke";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_joke);
+
+        Intent intent = getIntent();
+        if (intent.hasExtra(EXTRA_JOKE)){
+            TextView jokeView = findViewById(R.id.joke_view);
+            jokeView.setText(intent.getStringExtra(EXTRA_JOKE));
+        }
     }
 }
